c5608880d255ee8f51b578f49b534af03467e855
Ethan Epperly <eepperly@caltech.edu>
2025-04-06 11:58:08 -0700

    Update SL row-norm estimators

--- code/sl3.m
+++ code/sl3.m
@@ -1,11 +1,17 @@
 function srn = sl3(B,Bt,m,n,s)
-    k = floor(s/3);
-    Om = random_signs(m,k);
-    Y = Bt(Om);
-    Q = orth(Y);
-    BQ = B(Q);
-    k = s - 2*k;
-    Ga = random_signs(n,k);
-    Ga = Ga - Q * (Q'*Ga);
-    srn = vecnorm(BQ,2,2).^2 + vecnorm(B(Ga),2,2).^2/k;
+% Input:  Functions B() and Bt() computing matrix products B(X) = 
+%         B*X and Bt(X) = B'*X, dimensions m and n, and number of
+%         matvecs s
+% Output: Estimate srn of the squared row norms of B
+
+k = floor(s/3);                             % Approximation rank s/3
+Om = random_signs(m,k);                     % Matrix of random signs
+Y = Bt(Om);                                 % Matvecs with B'
+[Q,~] = qr(Y,"econ");                       % Orthonormalize                         
+BQ = B(Q);                                  % Matvecs with B
+k = s - 2*k;                                % Remaining matvecs
+Ga = random_signs(n,k);                     % Matrix of random signs
+Ga = Ga - Q * (Q'*Ga);                      % Orthogonalize against Q
+srn = sqrownorms(BQ) + sqrownorms(B(Ga))/k; % SL3 estimator
+
 end
