561233bdcf57077365fc1e4225c080657fc5be5b
Ethan Epperly <eepperly@caltech.edu>
2025-08-26 15:12:53 -0700

    Fix comment in diagprod

--- code/diagprod.m
+++ code/diagprod.m
@@ -1,6 +1,6 @@
 function d = diagprod(F,G)
 % Input:  Matrices F and G of the same size
-% Output: The diagonal of the product d = diag(G'*F)
+% Output: The diagonal of the product d = diag(F'*G)
 
 d = sum(conj(F).*G,1).';
 
